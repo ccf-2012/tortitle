43619af6a9563492e1cc98dbc6cfd6937438ec82
ccf-2012 <crlf66@163.com>
2025-09-07 13:38:44 +0800

    manual refracting

--- tortitle/tortitle.py
+++ tortitle/tortitle.py
@@ -75,20 +75,25 @@ class TorTitle:
         self.parse()
 
     def parse(self):
-        self._handle_bracket_title()
-        parsing_target = self.raw_name
-        if self.title != self.raw_name:
-            parsing_target = self.title
-        self._prepare_title()
-        self._extract_year()
-        self._extract_type()
-        self._extract_titles()
-        self._polish_title()
-        # self._handle_special_cases()
+        self.raw_name = self.title
+        self.title, self.cntitle = self._handle_bracket_title(self.title)
+
         self.media_source, self.video, self.audio = self._parse_more(self.raw_name)
-        self.group = self._parse_group(parsing_target)
         self.resolution = self._parse_resolution(self.raw_name)
-        # self.full_season = (self.type == 'tv') and (self.episode == '')
+        self.type = self._check_tv_type(delimer_to_space(self.raw_name))
+
+        self.title = self._prepare_title(self.title)
+
+        self.group = self._parse_group(self.title)
+        yearpos, self.year = self._extract_year(self.title)
+        se_pos = self._extract_season_episdoe(self.title)
+        self.failsafe_title = self.title
+        self.title = self._cut_s_year_season(self.title, yearpos, se_pos)
+        self.title = self._cut_s_keyword(self.title)
+
+        if not self.cntitle:
+            self.title, self.cntitle = self._extract_cntitle(self.title) 
+        self._polish_title()
 
 
     def _parse_more(self, torrent_name):
@@ -131,17 +136,19 @@ class TorTitle:
 
         return None
         
-    def _prepare_title(self):
-        self.title = cut_ext(self.title)
-        self.title = re.sub(r'^[「【][^】」]*[】」]', '', self.title, flags=re.I)
-        self.title = re.sub(r'^\w+TV-?(\d+)?([48]K)?\b', '', self.title, flags=re.I)
-        # if re.search(r"\d+x\d+", self.title, flags=re.I):
-        #     self.title = re.sub(r'^\d{4}[\s\.]', '', self.title, flags=re.I)
-        self.title = delimer_to_space(self.title)
+    def _prepare_title(self, processing_title):
+        processing_title = cut_ext(processing_title)
+        processing_title = re.sub(r'^[「【][^】」]*[】」]', '', processing_title, flags=re.I)
+        processing_title = re.sub(r'^\w+TV-?(\d+)?([48]K)?\b', '', processing_title, flags=re.I)
+        # if re.search(r"\d+x\d+", processing_title, flags=re.I):
+        #     processing_title = re.sub(r'^\d{4}[\s\.]', '', processing_title, flags=re.I)
+        processing_title = delimer_to_space(processing_title)
+        return processing_title
 
-    def _handle_bracket_title(self):
-        if self.title.startswith('[') and self.title.endswith(']'):
-            parts = [part.strip() for part in self.title[1:-1].split('][') if part.strip()]
+    def _handle_bracket_title(self, processing_title):
+        cntitle = ""
+        if processing_title.startswith('[') and processing_title.endswith(']'):
+            parts = [part.strip() for part in processing_title[1:-1].split('][') if part.strip()]
             keyword_pattern = r'1080p|2160p|4K|Web-?DL|720p|H\.?26[45]|x26[45]|全.{1,4}集'
             
             main_part = ''
@@ -156,21 +163,25 @@ class TorTitle:
                 if re.match(r'^'+keyword_pattern+'$', main_part, flags=re.I):
                     if keyword_idx > 0:
                         keyword_idx = keyword_idx - 1
-                        self.title = parts[keyword_idx]
+                        processing_title = parts[keyword_idx]
                 else:
-                    self.title = main_part
+                    processing_title = main_part
                 if keyword_idx > 0 and contains_cjk(parts[keyword_idx-1]):
                     full_cntitle = parts[keyword_idx-1]
                     full_cntitle = re.sub(r'大陆|港台', '', full_cntitle, flags=re.I)
-                    self.cntitle = full_cntitle.split(' ')[0].strip()
+                    cntitle = full_cntitle.split(' ')[0].strip()
+        return processing_title, cntitle
 
-    def _extract_year(self):
-        potential_years = re.findall(r'(19\d{2}|20\d{2})(?:\d{4})?\b', self.raw_name)
+    def _extract_year(self, processing_title):
+        _year_pos = 0
+        year = ""
+        potential_years = re.findall(r'(19\d{2}|20\d{2})(?:\d{4})?\b', processing_title)
         if potential_years:
-            self.year = potential_years[-1]
-            self._year_pos = self.title.rfind(self.year)
+            year = potential_years[-1]
+            _year_pos = processing_title.rfind(year)
+        return _year_pos, year
 
-    def _extract_type(self):
+    def _match_season(self, processing_title, match_key=None):
         patterns = {
             's_e': r'\b(S(\d+))(E(\d+)(-Ep?(\d+))?)\b',
             'season_only': r'(?<![a-zA-Z])(S(\d+)([\-\+]S?(\d+))?)\b(?!.*\bS\d+)',
@@ -180,100 +191,103 @@ class TorTitle:
             'cn_episode': r'第([一二三四五六七八九十]+|\d+)集',
             'full_season': r'[全]\w{,4}\s*[集季]'
         }
+        if match_key:
+            return re.search(patterns[match_key], processing_title)
+        else:
+            for key, pattern in patterns.items():
+                match = re.search(pattern, processing_title, flags=re.IGNORECASE)
+                if match:
+                    return key, match
+            return None, None
 
-        for key, pattern in patterns.items():
-            match = re.search(pattern, self.raw_name, flags=re.IGNORECASE)
-            if match:
-                self.type = 'tv'
-                break
-        if match := re.search(patterns['full_season'], self.raw_name):
-            self.full_season = True
-            
-        for key, pattern in patterns.items():
-            match = re.search(pattern, self.title, flags=re.IGNORECASE)
-            if match:
-                self.type = 'tv'
-                if key in ['s_e']:
-                    # self.season_int = int(match.group(1))
-                    # self.episode_int = int(match.group(2))
-                    self.season = match.group(1)
-                    self.episode = match.group(3)
+    def _check_tv_type(self, processing_title):
+        category = 'movie'
+        key, match = self._match_season(processing_title)
+        if match:
+            category = 'tv'
+            # if self._match_season(processing_title, 'full_season'):
+            #     full_season = True
+        return category
+
+    def _extract_season_episdoe(self, processing_title):
+        se_pos = 0
+        key, match = self._match_season(processing_title)
+        if match:
+            if key in ['s_e']:
+                # self.season_int = int(match.group(1))
+                # self.episode_int = int(match.group(2))
+                self.season = match.group(1)
+                self.episode = match.group(3)
+                self.seasons = [int(match.group(2))]
+                if match.group(6):
+                    self.episodes = list(range(int(match.group(4)), int(match.group(6))+1))
+                else:
+                    self.episodes = [int(match.group(4))]
+            elif key == 'season_only':
+                # self.season_int = tryint(match.group(1))
+                self.season = match.group(0)
+                if match.group(4):
+                    self.seasons = list(range(int(match.group(2)), int(match.group(4))+1))
+                else:
                     self.seasons = [int(match.group(2))]
-                    if match.group(6):
-                        self.episodes = list(range(int(match.group(4)), int(match.group(6))+1))
-                    else:
-                        self.episodes = [int(match.group(4))]
-                elif key == 'season_only':
-                    # self.season_int = tryint(match.group(1))
-                    self.season = match.group(0)
-                    if match.group(4):
-                        self.seasons = list(range(int(match.group(2)), int(match.group(4))+1))
-                    else:
-                        self.seasons = [int(match.group(2))]
-                elif key in ['season_word', 'cn_season']:
-                    # self.season_int = tryint(match.group(1))
-                    season_int = tryint(match.group(1))
-                    self.seasons = [season_int]
-                    self.season = 'S'+ str(season_int).zfill(2) if season_int else ''
-                elif key in ['cn_episode', 'ep_only']:
-                    self.season = 'S01'
-                    self.seasons = [1]
-                    if match.group(3):
-                        self.episodes = list(range(tryint(match.group(1)), tryint(match.group(3))+1))
-                    else:
-                        self.episodes = [tryint(match.group(1))]
+            elif key in ['season_word', 'cn_season']:
+                # self.season_int = tryint(match.group(1))
+                season_int = tryint(match.group(1))
+                self.seasons = [season_int]
+                self.season = 'S'+ str(season_int).zfill(2) if season_int else ''
+            elif key in ['cn_episode', 'ep_only']:
+                self.season = 'S01'
+                self.seasons = [1]
+                if match.group(3):
+                    self.episodes = list(range(tryint(match.group(1)), tryint(match.group(3))+1))
+                else:
+                    self.episodes = [tryint(match.group(1))]
 
-                    self.episode = match.group()
-                elif key == 'full_season':
-                    self.full_season = True
+                self.episode = match.group()
+            elif key == 'full_season':
+                self.full_season = True
 
-                self._se_pos = match.span(0)[0]
-                return
+            se_pos = match.span(0)[0]
+        return se_pos
 
-    def _cut_s_year_season(self):
-        positions = [p for p in [self._year_pos, self._se_pos] if p > 0]
+    def _cut_s_year_season(self, processing_title, year_pos, se_pos):
+        positions = [p for p in [year_pos, se_pos] if p > 0]
         if not positions:
-            if try_match := re.search(r"(\d+x\d+|BDRip|.26[45])", self.title, flags=re.I):
+            if try_match := re.search(r"(\d+x\d+|BDRip|.26[45])", processing_title, flags=re.I):
                 positions = [try_match.span(0)[0]]
         if positions:
             cut_pos = min(positions)
-            self.title = self.title[:cut_pos]
-        self.title = self.title.strip()
+            processing_title = processing_title[:cut_pos]
+        return processing_title.strip()
 
-    def _cut_s_keyword(self):
+    def _cut_s_keyword(self, processing_title):
         tags = [
             '2160p', '1080p', '720p', '480p', 'BluRay', r'(4K)?\s*Remux', 
             r'WEB-?(DL)?', r'(?<![a-z])4K', r'(?<=\w\s)BDMV',
         ]
         pattern = r'(' + '|'.join(tag for tag in tags) + r')\b.*$'
-        self.title = re.sub(pattern, '', self.title, flags=re.IGNORECASE)
-        self.title = self.title.strip()
+        processing_title = re.sub(pattern, '', processing_title, flags=re.IGNORECASE)
+        return processing_title.strip()
     
-    def _extract_titles(self):
-        self.failsafe_title = self.title
-        self._cut_s_year_season()
-        self._cut_s_keyword()
+    def _extract_cntitle(self, processing_title):
+        cntitle = ""
+        if contains_cjk(processing_title):
+            cntitle = processing_title
+            if m := re.search(r"([一-鿆]+[\-0-9a-zA-Z]*)[ :：]+([^一-鿆]+\b)", processing_title, flags=re.I):
+                cntitle = cntitle[:m.span(1)[1]]
+                processing_title = m.group(2)
 
-        if not self.cntitle:
-            if contains_cjk(self.title):
-                self.cntitle = self.title
-                if m := re.search(r"([一-鿆]+[\-0-9a-zA-Z]*)[ :：]+([^一-鿆]+\b)", self.title, flags=re.I):
-                    self.cntitle = self.cntitle[:m.span(1)[1]]
-                    self.title = m.group(2)
+            # 删去：汉字之前，有空格分隔的 ascii 字符串
+            if m1 := re.match(r'^([^一-鿆]*)[\s\(\[]+[一-鿆]', cntitle, flags=re.I):
+                cntitle = cntitle.replace(m1.group(1), '').strip()
 
-                # 删去：汉字之前，有空格分隔的 ascii 字符串
-                if m1 := re.match(r'^([^一-鿆]*)[\s\(\[]+[一-鿆]', self.cntitle, flags=re.I):
-                    self.cntitle = self.cntitle.replace(m1.group(1), '').strip()
+            # 取汉字串中第一个空格前部分
+            if cntitle:
+                match = re.match(r'^([^ \-\(\[]*)', cntitle)
+                if match:
+                    cntitle = match.group()
 
-                # 取汉字串中第一个空格前部分
-                if self.cntitle:
-                    match = re.match(r'^([^ \-\(\[]*)', self.cntitle)
-                    if match:
-                        self.cntitle = match.group()
-
-        self.title = self.title.strip()
-
-        return
+        return processing_title.strip(), cntitle
 
     def _check_title(self):
         m1 = re.search('[a-zA-Z]', self.title)
